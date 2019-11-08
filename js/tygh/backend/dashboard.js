a1478961613febd7c438db038935d7c55dbc0e80
ShiddenS <lenok-flauv@mail.ru>
2019-11-08 16:32:39 +0300

    test

--- js/tygh/backend/dashboard.js
+++
@@ -1,8 +0,0 @@
-(function (_, $) {
-    $(_.doc).ready(function () {
-        $.ceAjax('request', fn_url('index.index'), {
-            result_ids: 'dashboard_content,actions_panel',
-            hidden: true
-        });
-    });
-})(Tygh, Tygh.$);
